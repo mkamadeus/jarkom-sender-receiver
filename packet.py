1f808d27981d7dfa6e385b374302a313bd2b81e6
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-09-26 00:33:08 +0700

    :fire: Fixed integer not represented correctly in packets

--- packet.py
+++ packet.py
@@ -1,5 +1,5 @@
 import math
-from utility import bytearray_to_binary
+from utility import bytearray_to_binary, bytearray_to_int, int_to_bytes
 
 class Packet:
 
@@ -10,8 +10,8 @@ class Packet:
   # Data = 32767 bytes (2^15-1)
   def __init__(self, data_type, data_length, seqnum, data):
     self.data_type = bytearray(data_type, 'utf-8')
-    self.data_length = bytearray(str(data_length), 'utf-8')
-    self.seqnum = bytearray(str(seqnum), 'utf-8')
+    self.data_length = bytearray(data_length)
+    self.seqnum = bytearray(seqnum)
     self.data = bytearray(data, 'utf-8') + (b'\0'*(32767 - len(data)))
     self.checksum = self.generate_checksum()
 
@@ -19,27 +19,33 @@ class Packet:
     content = self.data_type + self.data_length + self.seqnum + self.data
     checksum = bytearray('\0\0', 'utf-8')
     for i in range(0, math.ceil((5 + self.get_data_length())/2)):
-      print(bytearray_to_binary(checksum))
-      print(bytearray_to_binary(content[2*i:2*i+2]))
-      print('----------')
+      # print(bytearray_to_binary(checksum))
+      # print(bytearray_to_binary(content[2*i:2*i+2]))
+      # print('----------')
       checksum[0] ^= content[2*i]
       checksum[1] ^= content[2*i+1]
-      print(bytearray_to_binary(checksum))
-      print()
+      # print(bytearray_to_binary(checksum))
+      # print()
 
     return checksum
 
   def get_data_length(self):
-    return int(self.data_length.decode())
+    return bytearray_to_int(self.data_length)
 
-  def print_packet(self):
+  def print_packet_info(self):
     print(f'type = {bytearray_to_binary(self.data_type)} ({self.data_type.decode()})')
     print(f'length = {bytearray_to_binary(self.data_length)} ({self.data_length.decode()})')
     print(f'seqnum = {bytearray_to_binary(self.seqnum)} ({self.seqnum.decode()})')
-    print(f'checksum = {bytearray_to_binary(self.seqnum)} ({self.checksum.decode()})')
+    print(f'checksum = {bytearray_to_binary(self.checksum)}')
     print(f'data = {bytearray_to_binary(self.data[0:self.get_data_length()])} ({self.data[0:self.get_data_length()].decode()})')
-    print()
-    # print(f'{bytearray_to_binary(self.data_type)} {bytearray_to_binary(self.data_length)} {bytearray_to_binary(self.seqnum)} {bytearray_to_binary(self.seqnum)} {bytearray_to_binary(self.data)}')
 
-p = Packet('i',3,1,'ac')
-p.print_packet()
+  def print_packet_data(self):  
+    print(f'{bytearray_to_binary(self.data_type)} {bytearray_to_binary(self.data_length)} {bytearray_to_binary(self.seqnum)} {bytearray_to_binary(self.checksum)} {bytearray_to_binary(self.data[0:self.get_data_length()])}')
+
+
+
+message = 'Lorem ipsum dolor sit amet'
+print(int_to_bytes(3))
+p = Packet('i',int_to_bytes(len(message)),int_to_bytes(1), message)
+p.print_packet_info()
+p.print_packet_data()
--- utility.py
+++ utility.py
@@ -1,2 +1,8 @@
-def bytearray_to_binary(s):
-  return ' '.join('{0:08b}'.format(c, 'b') for c in s)
+def bytearray_to_binary(ba):
+  return ' '.join('{0:08b}'.format(c, 'b') for c in ba)
+
+def bytearray_to_int(ba):
+  return int.from_bytes(ba, byteorder="big")
+
+def int_to_bytes(n):
+  return bytes([n])
