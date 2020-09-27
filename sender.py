72cbe4386dbef1f9e50294377921af32e46a5a88
anindya <pamsrewari@gmail.com>
2020-09-27 22:10:33 +0700

    :sparkles: Add timeout

--- sender.py
+++ sender.py
@@ -5,18 +5,20 @@ port = int(input())
 
 # Create UDP based socket (using Datagram Sockets)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(3)
 server_address =('localhost', 3000)
-message = b'lorem ipsum dolor sit amet'
+message = b'lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum dolor sit amet'
 
+data = ''
+while not data:
+    try:
+      print(f'sending')
+      sent = s.sendto(message, server_address)
 
-try:
-  print(f'sending')
-  sent = s.sendto(message, server_address)
+      print(f'waiting to receive')
+      data, server = s.recvfrom(4096)
 
-  print(f'waiting to receive')
-  data, server = s.recvfrom(4096)
+      print(f'received {data}')
 
-  print(f'received {data}')
-
-finally:
-  s.close()
+    except socket.timeout:
+      print(f'time out!')
