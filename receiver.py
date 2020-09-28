2c7d3055dc7388efd515ffa34d9f24a2b56cdad8
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-09-28 20:49:56 +0700

    File segmentation finished

--- receiver.py
+++ receiver.py
@@ -5,7 +5,7 @@ OUTFILE = "./out/downloaded"
 
 # Input port
 # port = int(input())
-port = 3000
+port = int(input())
 
 # Create UDP based socket (using Datagram Sockets)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,24 +13,30 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind(('localhost', port))
 print(f'socket binded to {port}')
 
+content = b''
 while True:
   data, address = s.recvfrom(32774)
   print(f'Received {len(data)} bytes from {address}')
   
   if(data):
     p = Packet(byte_data=data)
-    p.print_packet_info()
+
+    print(len(content))
+    content += p.get_message()
 
     # Create ACK packet
-    ack = Packet(b'\x01', p.get_seq_num(), b'')
+    ack = Packet(b'\x01' if p.packet_type != b'\x02' else b'\x03', p.get_seq_num(), b'')
 
     sent = s.sendto(ack.get_packet_content(), address)
-    print(f'sent {len(data)} back to {address}')
+    print(f'Sent {len(data)} back to {address}')
 
-    with open(OUTFILE, 'wb') as f:
-      f.write(p.get_message())
-  # client, address = s.accept()
-  # print(f'Got connection from {address}')
+
+    if(p.packet_type == b'\x02'):
+      break
+
+
+
+with open(OUTFILE, 'wb') as f:
+  f.write(content)
+
 s.close()
-  # client.send('yey')
-  # client.close()
--- sender.py
+++ sender.py
@@ -14,6 +14,8 @@ filename = input()
 with open(filename, 'rb') as f:
   message = f.read()
 
+print(len(message))
+
 splitted = [message[i:i+32767] for i in range(0, len(message), 32767)]
 
 # Create UDP based socket (using Datagram Sockets)
@@ -21,26 +23,23 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(3)
 server_address =('localhost', 3000)
 
-seqnum = 0
-for split in splitted:
+for seqnum, split in enumerate(splitted):
   data = ''
   while not data:
     try:
-      print(f'sending')
-      p = Packet(b'\x00', seqnum, split)
-      p.print_packet_info()
+      print(f'Sending packet {seqnum}')
+      p = Packet(b'\x00' if len(splitted)-1 != seqnum else b'\x02', seqnum, split)
 
       sent = s.sendto(p.get_packet_content(), server_address)
 
-      print(f'waiting to receive')
+      print(f'Waiting to receive')
       data, server = s.recvfrom(32774)
 
       p = Packet(byte_data=data)
       if(p.packet_type == b'\x01'):
-        print(f'received ACK')
-        seqnum += 1
+        print(f'Received ACK')
 
     except socket.timeout:
-      print(f'time out!')
+      print(f'Time out!')
 
 s.close()
