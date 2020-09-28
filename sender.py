ad3c26165e1c101acc047d6c17eb8b7be882d8d9
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-09-28 22:25:52 +0700

    Added multiple addresses support

--- sender.py
+++ sender.py
@@ -18,29 +18,30 @@ chunks = [message[i:i+32767] for i in range(0, len(message), 32767)]
 # Create UDP based socket (using Datagram Sockets)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(3)
-server_address =('localhost', 3000)
+server_addresses =[(ip, port) for ip in address.split()]
 
 # For each chunk...
-for seq_num, chunk in enumerate(chunks):
-  data = None
-  while data is None:
-    try:
-      # Send file segment
-      print(f'Sending packet {seq_num}')
-      p = Packet(b'\x00' if len(chunks)-1 != seq_num else b'\x02', seq_num, chunk)
-      sent = s.sendto(p.get_packet_content(), server_address)
+for server_address in server_addresses:
+  for seq_num, chunk in enumerate(chunks):
+    data = None
+    while data is None:
+      try:
+        # Send file segment
+        print(f'Sending packet {seq_num}')
+        p = Packet(b'\x00' if len(chunks)-1 != seq_num else b'\x02', seq_num, chunk)
+        sent = s.sendto(p.get_packet_content(), server_address)
 
-      # Wait to receive ACK Packet
-      print(f'Waiting to receive')
-      data, server = s.recvfrom(32774)
-      p = Packet(byte_data=data)
+        # Wait to receive ACK Packet
+        print(f'Waiting to receive')
+        data, server = s.recvfrom(32774)
+        p = Packet(byte_data=data)
 
-      # If received packet is ACK (\x01)
-      if(p.packet_type == b'\x01'):
-        print(f'Received ACK')
+        # If received packet is ACK (\x01)
+        if(p.packet_type == b'\x01'):
+          print(f'Received ACK')
 
-    except socket.timeout:
-      # If ACK not received (packet loss, ack loss)
-      print(f'Time out!')
+      except socket.timeout:
+        # If ACK not received (packet loss, ack loss)
+        print(f'Time out!')
 
 s.close()
