573d869bffcb87757b938ec2d6b98e3657da15f9
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-09-28 21:48:27 +0700

    :sparkles: Comments on sender

--- sender.py
+++ sender.py
@@ -1,6 +1,5 @@
-import socket
 from packet import Packet
-from utility import bytearray_to_binary, bytearray_to_int, int_to_bytes, bytes_to_binary, bytes_to_int
+import socket
 
 # Input address receiver
 address = input() 
@@ -8,38 +7,40 @@ address = input()
 # Input port
 port = int(input())
 
-# Input file name
+# Input and open file
 filename = input()
-
 with open(filename, 'rb') as f:
   message = f.read()
 
-print(len(message))
-
-splitted = [message[i:i+32767] for i in range(0, len(message), 32767)]
+# Split message into smaller chunks
+chunks = [message[i:i+32767] for i in range(0, len(message), 32767)]
 
 # Create UDP based socket (using Datagram Sockets)
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(3)
 server_address =('localhost', 3000)
 
-for seqnum, split in enumerate(splitted):
-  data = ''
-  while not data:
+# For each chunk...
+for seq_num, chunk in enumerate(chunks):
+  data = None
+  while data is None:
     try:
-      print(f'Sending packet {seqnum}')
-      p = Packet(b'\x00' if len(splitted)-1 != seqnum else b'\x02', seqnum, split)
-
+      # Send file segment
+      print(f'Sending packet {seq_num}')
+      p = Packet(b'\x00' if len(chunks)-1 != seq_num else b'\x02', seq_num, chunk)
       sent = s.sendto(p.get_packet_content(), server_address)
 
+      # Wait to receive ACK Packet
       print(f'Waiting to receive')
       data, server = s.recvfrom(32774)
-
       p = Packet(byte_data=data)
+
+      # If received packet is ACK (\x01)
       if(p.packet_type == b'\x01'):
         print(f'Received ACK')
 
     except socket.timeout:
+      # If ACK not received (packet loss, ack loss)
       print(f'Time out!')
 
 s.close()
