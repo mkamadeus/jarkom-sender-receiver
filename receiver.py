59a677785a1c9935da75b971bbfcf41776d2e066
Matthew Kevin Amadeus <13518035@std.stei.itb.ac.id>
2020-09-29 10:14:21 +0700

    Fixing ACK FINACK

--- receiver.py
+++ receiver.py
@@ -1,8 +1,8 @@
 import socket
 from packet import Packet
 import logging
-# import time
-# import random
+import time
+import random
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='[%H:%M:%S]', level=logging.INFO)
 
@@ -19,7 +19,12 @@ server_address =(socket.gethostbyname(socket.gethostname()), port)
 s.bind(server_address)
 logging.info(f'Socket binded to {server_address}')
 
-queue = []
+# content = b''
+last_seq_num = -1
+last_data = b''
+# Reset file
+open(outfile, 'w').close()
+
 while True:
   data, address = s.recvfrom(32774)
   logging.info(f'Received {len(data)} bytes from {address}')
@@ -27,29 +32,46 @@ while True:
   if(data):
     p = Packet(byte_data=data)
 
-    if (p.generate_checksum() == p.get_checksum()):
-      logging.info("Checksum matched")
-      queue.append((p.seq_num, p.get_message())) 
+    if(p.get_seq_num() > last_seq_num):
+      logging.info(f'Writing data from packet {last_seq_num}')
+      last_seq_num = p.get_seq_num()
 
-      # Create ACK packet
-      ack = Packet(b'\x01' if p.packet_type != b'\x02' else b'\x03', p.get_seq_num(), b'')
-      # time.sleep(random.random()*6)
-      sent = s.sendto(ack.get_packet_content(), address)
-      logging.info(f'Sent ACK to {address}')
+      with open(outfile, 'ab') as f:
+        f.write(last_data)
+
+      last_data = p.get_message()
+    
+
+    if (p.generate_checksum() == p.get_checksum()):
+      logging.info(f'Checksum matched for packet {p.get_seq_num()}')
+
+      # Delay for testing
+      time.sleep(random.random()*10)
+
+      if(p.packet_type == b'\x02'):
+        # Create FIN-ACK packet
+        ack = Packet(b'\x03', p.get_seq_num(), b'')
+        sent = s.sendto(ack.get_packet_content(), address)
+        logging.info(f'Sent FIN-ACK to {address}')
+      # with open(outfile, 'r+b') as f:
+      #   f.write(last_data)
+      else:
+        # Create ACK packet
+        ack = Packet(b'\x01', p.get_seq_num(), b'')
+        sent = s.sendto(ack.get_packet_content(), address)
+        logging.info(f'Sent ACK to {address}')
+      # ack = Packet(b'\x01' if p.packet_type != b'\x02' else b'\x03', p.get_seq_num(), b'')
+      # sent = s.sendto(ack.get_packet_content(), address)
+      # logging.info(f'Sent ACK to {address}')
 
     else:
       logging.info("Checksum mismatch, ACK not sent")
       
-    if(p.packet_type == b'\x02'):
-      logging.info("FIN packet found, stopping")
-      break
+    # if(p.packet_type == b'\x02'):
+    #   logging.info("FIN packet found")
+      # break
 
-queue.sort()
-content = b''
-for _,chunk in queue:
-  content += chunk
-
-with open(outfile, 'wb') as f:
-  f.write(content)
+# with open(outfile, 'wb') as f:
+#   f.write(content)
 
 s.close()
--- sender.py
+++ sender.py
@@ -25,27 +25,45 @@ s.settimeout(5)
 server_addresses =[(socket.gethostbyname(add), port) for add in address.split(',')]
 
 # For each chunk...
+seq_num = 0
 for server_address in server_addresses:
-  for seq_num, chunk in enumerate(chunks):
+  received_fin_ack = False
+  while(not received_fin_ack):
     data = None
     while data is None:
       try:
-        # Send file segment
-        logging.info(f'Sending packet {seq_num}')
-        p = Packet(b'\x00' if len(chunks)-1 != seq_num else b'\x02', seq_num, chunk)
-        sent = s.sendto(p.get_packet_content(), server_address)
+        # If already last chunk
+        if(len(chunks)-1 == seq_num):
+          logging.info(f'Sending packet {seq_num} (FIN)')
+          # Send file segment
+          p = Packet(b'\x00' if len(chunks)-1 != seq_num else b'\x02', seq_num, chunks[seq_num])
+          sent = s.sendto(p.get_packet_content(), server_address)
 
-        # Wait to receive ACK Packet
-        logging.info(f'Waiting to receive ACK')
-        data, server = s.recvfrom(32774)
-        p = Packet(byte_data=data)
+          # Wait to receive FIN-ACK Packet
+          logging.info(f'Waiting to receive FIN-ACK')
+          data, server = s.recvfrom(32774)
+          p = Packet(byte_data=data)
 
-        # If received packet is ACK (\x01)
-        if(p.packet_type == b'\x01'):
-          logging.info(f'Received ACK')
-        
-        if(p.packet_type == b'\x03'):
-          logging.info(f'Received FIN-ACK')
+          # If received FIN-ACK
+          if(p.packet_type == b'\x03'):
+            logging.info(f'Received FIN-ACK')
+            received_fin_ack = True
+        else:
+          logging.info(f'Sending packet {seq_num}')
+          # Send file segment
+          p = Packet(b'\x00', seq_num, chunks[seq_num])
+          sent = s.sendto(p.get_packet_content(), server_address)
+          
+          # Wait to receive ACK Packet
+          logging.info(f'Waiting to receive ACK')
+          data, server = s.recvfrom(32774)
+          p = Packet(byte_data=data)
+
+          # If received packet is ACK (\x01)
+          if(p.packet_type == b'\x01'):
+            logging.info(f'Received ACK')
+            seq_num += 1
+
 
       except socket.timeout:
         # If ACK not received (packet loss, ack loss)
